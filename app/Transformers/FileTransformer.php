64210118a09ed7ab20723aa3a0ebc24d47719ae7
falaqin <falaqin.work@gmail.com>
2023-10-30 13:37:34 +0800

    added file transformer class for endpoints

---
+++ app/Transformers/FileTransformer.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Transformers;
+
+use App\Models\UploadedFile;
+
+class FileTransformer
+{
+    public function transform(UploadedFile $file)
+    {
+        return [];
+    }
+}
